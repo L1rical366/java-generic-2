be382500002821b8e06e0b6f920ed69cf0a3d289
L1rical366 <71489600+L1rical366@users.noreply.github.com>
2021-01-03 20:12:41 +0200

    Add files via upload
    generic -2

---
+++ src/main/java/org/itstep/List.java
@@ -0,0 +1,44 @@
+package org.itstep;
+
+import java.util.Arrays;
+
+// FIXME: обобщенный тип должен быть классом расширяющим интерфейс Comparable<T>
+class List<T extends Comparable<T>>{
+    private T[] arrays;
+
+    public List() {
+        // FIXME: создать массив типов Comparable
+        this.arrays = (T[]) new Comparable[0];
+    }
+
+    public void add(T item) {
+        arrays = Arrays.copyOf(arrays, arrays.length + 1);
+        arrays[arrays.length - 1] = item;
+    }
+
+    public T get(int idx) {
+        return arrays[idx];
+    }
+
+    public T min() {
+        T min = arrays[0];
+        // FIXME: реализовать поиск наименьшего элемента используя метод compareTo интерфейса IComparable<T>
+        for (T array : arrays) {
+            if (array.compareTo(min) < 0) {
+                min = array;
+            }
+        }
+        return min;
+    }
+
+    public T max() {
+        T max = arrays[0];
+        // FIXME: реализовать поиск наибольшего элемента используя метод compareTo интерфейса IComparable<T>
+        for (T array : arrays) {
+            if (array.compareTo(max) > 0) {
+                max = array;
+            }
+        }
+        return max;
+    }
+}
